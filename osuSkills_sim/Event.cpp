beae6fb54864aa9831196d8515012a098cd9a1f2
abraker95 <abraker95@gmail.com>
2016-04-17 03:08:33 -0400

    [ADD] Keyboard functionality

--- osuSkills_sim/Event.cpp
+++ osuSkills_sim/Event.cpp
@@ -1,6 +1,10 @@
 #include "Event.h"
 
-EventReceiver::EventReceiver(){}
+EventReceiver::EventReceiver()
+{
+	for (u32 i = 0; i<KEY_KEY_CODES_COUNT; ++i)
+		KeyIsDown[i] = false;
+}
 
 bool EventReceiver::OnEvent(const SEvent& event)
 {
@@ -33,7 +37,13 @@ bool EventReceiver::OnEvent(const SEvent& event)
 			default:
 				// Wheel
 				break;
-		}
+		}		
+	}
+
+	// Remember whether each key is down or up
+	if (event.EventType == EET_KEY_INPUT_EVENT)
+	{
+		KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
 	}
 
 	return false;
@@ -59,4 +69,10 @@ int EventReceiver::getWheel(bool _mode)
 		MouseState.wheelPrev = MouseState.wheelCurr;
 		return MouseState.wheelMove;
 	}	
+}
+
+// This is used to check whether a key is being held down
+bool EventReceiver::IsKeyDown(EKEY_CODE keyCode) const
+{
+	return KeyIsDown[keyCode];
 }
--- osuSkills_sim/Event.h
+++ osuSkills_sim/Event.h
@@ -20,13 +20,16 @@ class EventReceiver : public IEventReceiver
 			SMouseState() : LeftButtonDown(false), LeftButtonEdge(false) { }
 		};
 
-
 		virtual bool OnEvent(const SEvent& event);
+		virtual bool IsKeyDown(EKEY_CODE keyCode) const;
 		const SMouseState &GetMouseState(void) const;
 		int getWheel(bool _mode = false); // _mode = 0: 0 if no change,  _mode = 1: get the last value
 
 	private:
 		SMouseState MouseState;
+
+		// We use this array to store the current state of each key
+		bool KeyIsDown[KEY_KEY_CODES_COUNT];
 };
 
 #endif
