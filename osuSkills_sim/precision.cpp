3a95bb26b39e0cb735d8e504c4fba26fc69cc834
abraker95 <abraker95@gmail.com>
2016-05-07 23:37:18 -0400

    [ADD] Experimental formula for the precision skill

--- osuSkills_sim/precision.cpp
+++ osuSkills_sim/precision.cpp
@@ -1,5 +1,28 @@
 #include "precision.h"
 #include "analysis.h"
+#include "Geometry.h"
+#include "osuCalc.h"
+
+#include <iostream>
+
+double pattern2skill(std::vector<std::tuple<int, int, int, int>>& _pattern, double _CS)
+{
+	double dist = getDist(position2di(std::get<Hitcircle::XPOS>(_pattern[0]), std::get<Hitcircle::YPOS>(_pattern[0])),
+				   	      position2di(std::get<Hitcircle::XPOS>(_pattern[1]), std::get<Hitcircle::YPOS>(_pattern[1])));
+	double velocity = dist / (abs(std::get<Hitcircle::TIME>(_pattern[1]) - std::get<Hitcircle::TIME>(_pattern[0])));
+	double circleSize = CS2px(_CS);
+	if (dist < circleSize)
+		dist = 0.0;
+	else if (dist == 0.0)
+		dist = 1.0;
+	else
+		dist = 3.2/(0.1* circleSize - 1.4) * acos(circleSize/dist);
+	
+	velocity = 10*pow(velocity, 2);
+	circleSize = 3000 / pow(circleSize, 2);
+
+	return dist * velocity * circleSize;
+}
 
 double getPrecisionSkill(std::vector<Hitcircle> &_hitcircles, int _time, double _CS, double _AR, bool _hidden)
 {
@@ -17,14 +40,17 @@ double getPrecisionSkill(std::vector<Hitcircle> &_hitcircles, int _time, double 
 	else
 	{
 		std::vector<std::tuple<int, int, int, int>> pattern;
+		double dist = 0;
+		double velocity = 0;
 
 		if (_hitcircles[index - 1].isSlider())
 			if (_time < _hitcircles[index - 1].getEndTime())
 				index--;
 
-		pattern = getPattern(_hitcircles, _time, index, _CS, 3);
+		pattern = getPattern(_hitcircles, _time, index, _CS, 2);		
+		precision = 1.25 * pattern2skill(pattern, _CS);
 
-		// TODO
+		std::cout << precision << std::endl;
 	}
 
 	return precision;
