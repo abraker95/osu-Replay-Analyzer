4b4d2d7ada348c3de265b755267c2b8ddd4d1fcd
abraker95 <abraker95@gmail.com>
2016-05-07 21:46:57 -0400

    [ADD] Files for the agility skill

---
+++ osuSkills_sim/agility.cpp
@@ -0,0 +1,31 @@
+#include "agility.h"
+#include "analysis.h"
+
+double getControlSkill(std::vector<Hitcircle> &_hitcircles, int _time, double _CS, double _AR, bool _hidden)
+{
+	double agility = 0;
+	int index = getHitcircleAt(_hitcircles, _time) + 1;
+
+	if (index > _hitcircles.size() - 1) 	// if it's beyond the end
+	{
+		agility = 0;
+	}
+	else if (index < 3) // if it's at or before the beggining
+	{
+		agility = 0;
+	}
+	else
+	{
+		std::vector<std::tuple<int, int, int, int>> pattern;
+
+		if (_hitcircles[index - 1].isSlider())
+			if (_time < _hitcircles[index - 1].getEndTime())
+				index--;
+
+		pattern = getPattern(_hitcircles, _time, index, _CS, 3);
+
+		// TODO
+	}
+
+	return agility;
+}
