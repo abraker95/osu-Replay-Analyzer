85556c41165093e5657244dd21796eab913ca845
abraker95 <abraker95@gmail.com>
2016-08-06 18:27:27 -0400

    [FIX] Allow the button to be declared but not initialized

--- osu-Replay-Analyzer/ui/Button.cpp
+++ osu-Replay-Analyzer/ui/Button.cpp
@@ -1,6 +1,8 @@
 #include "Button.h"
 #include "../utils/mathUtils.h"
 
+Button::Button(GuiObj* _parent) : GuiObj(0, 0, 0, 0, _parent){}
+
 Button::Button(int _xpos, int _ypos, int _width, int _height, GuiObj* _parent) : GuiObj(_xpos, _ypos, _width, _height, _parent)
 {
 	pressed = false;
