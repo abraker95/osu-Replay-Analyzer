3e8f1c9d828b47b44669d1279f6a9a50eb058922
abraker95 <abraker95@gmail.com>
2016-08-28 22:16:13 -0400

    [FIX] Bye-bye! *drops nuke*  :D/

--- osu-Replay-Analyzer/osu!/Skills/osu-std/old/control.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-#include "../../Filestructure/Beatmap.h"
-
-double getControlSkill(Beatmap* _beatmap, int _time);
--- osu-Replay-Analyzer/osu!/Skills/osu-std/old/reading.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-#include "../../Filestructure/Beatmap.h"
-
-double getReadingSkill(Beatmap* _beatmap, int _time);
