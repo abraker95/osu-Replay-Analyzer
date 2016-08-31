fc527cdd4a1f8627e5149a6622c526bf10c26b49
abraker95 <abraker95@gmail.com>
2016-08-31 01:37:24 -0400

    [FIX] Inconsistencies within note rates; flawed data

--- osu-Replay-Analyzer/osu!/Analysis/Beatmap/NoteRatePerKeyPrecise.cpp
+++ osu-Replay-Analyzer/osu!/Analysis/Beatmap/NoteRatePerKeyPrecise.cpp
@@ -4,6 +4,8 @@
 #include "../../osu_standard.h"
 #include "../../osu_mania.h"
 
+#include "../AnalysisStruct.h"
+
 Analyzer_NoteRatePerKeyPrec::Analyzer_NoteRatePerKeyPrec() : Analyzer("max note rate for key (notes/s)") {}
 Analyzer_NoteRatePerKeyPrec::~Analyzer_NoteRatePerKeyPrec() {}
 
@@ -21,71 +23,43 @@ void Analyzer_NoteRatePerKeyPrec::AnalyzeMania(Play* _play)
 	std::vector<Hitobject*>& hitobjects = _play->beatmap->getHitobjects();
 	osu::TIMING timing;
 		timing.data = 0;
+		data.push_back({ hitobjects[0]->getTime() - 1, 0});
 
-	std::vector<int> i, previ, time;
-		i.resize(KEYS);
-		previ.resize(KEYS);
-		time.resize(KEYS);
+	Analyzer* analyzer = AnalysisStruct::beatmapAnalysis.getAnalyzer("note rate (notes/s)");
+	if (analyzer == nullptr) return;
 
-	// Setup the indices to have one in front of the other
-	for (int key = 0; key < KEYS; key++)
+	std::vector<osu::TIMING>& noteRates = *(analyzer->getData());
+	for (int i=0; i<noteRates.size(); i++)
 	{
-		OSUMANIA::getNextNoteOnColumn(_play, key, &i[key]);
-		OSUMANIA::getNextNoteOnColumn(_play, key, &i[key]);
-		
-		// Since starting index is 0, make sure we don't skip
-		// the first note that might happen to be on the column
-		if(OSUMANIA::getKey(hitobjects[0]->getPos().X, KEYS) != key)
-			OSUMANIA::getNextNoteOnColumn(_play, key, &previ[key]);
-	}
+		long currTime = noteRates[i].time;
 
-	while (true)
-	{
-		if (OSUMANIA::isAtEnd(KEYS, i, hitobjects)) break;
+		long minInterval = INT_MAX;
+		int recKey = -1;
 
-		// operate per-key
 		for (int key = 0; key < KEYS; key++)
 		{
-			// Check to see if this column came to the end
-			if (i[key] == OSUMANIA::MANIA_END) continue;
+			int currI = OSUMANIA::FindHitobjectAt(_play, currTime, key, false);
+			int nextI = OSUMANIA::getNextIndexOnColumn(hitobjects, key, KEYS, currI);
 
-			// Check to see if the time was already recorded
-			bool differentTime = false;
-			time[key] = hitobjects[i[key]]->getTime();
-			for (int key2 = 0; key2 < KEYS; key2++)
-				differentTime |= (time[key2] == time[key]);
+			if (currI == OSUMANIA::MANIA_END)	continue;
+			if (nextI == OSUMANIA::MANIA_END)	continue;
 
-			// Recond timing info
-			if (differentTime)
+			long interval = hitobjects[nextI]->getTime() - hitobjects[currI]->getTime();
+			if (minInterval > interval)
 			{
-				double timeInterval = time[key] - hitobjects[previ[key]]->getTime();
-				
-				// Irrlitch crashes when trying to convert inf -> str
-				if (timeInterval == 0) timing.data = INT_MAX;
-				else            	   timing.data = 1000.0/timeInterval;
-			
-				timing.pos = vector2df(OSUMANIA::getKey(hitobjects[i[key]]->getPos().X, KEYS), 0);
-				timing.press = false;
-				timing.time = time[key];
-
-				data.push_back(timing);
-			}
-
-			// Skip if this one is ahead of at least another column
-			bool get = OSUMANIA::isBehindOtherColumns(KEYS, key, i, hitobjects);
-			if (get == true)
-			{
-				OSUMANIA::getNextNoteOnColumn(_play, key, &i[key]);
-				OSUMANIA::getNextNoteOnColumn(_play, key, &previ[key]);
-			}
-			else
-			{
-				continue;
+				minInterval = interval;
+				recKey = key;
 			}
 		}
-	}
 
-	osu::SortByTime(data);
+		if (minInterval == INT_MAX) continue;
+
+		timing.time = currTime;
+		timing.data = 1000.0 / (double)minInterval;
+		timing.key = recKey;
+
+		data.push_back(timing);
+	}
 }
 
 void Analyzer_NoteRatePerKeyPrec::AnalyzeDodge(Play* _play)
