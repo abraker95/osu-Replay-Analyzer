61e63f77e8d20f1b0fc373fcea268f215b71cbd7
abraker95 <abraker95@gmail.com>
2016-08-31 01:31:50 -0400

    [FIX] Move includes to the cpp file to reduce compile times

--- osu-Replay-Analyzer/osu!/Analysis/AnalysisStruct.cpp
+++ osu-Replay-Analyzer/osu!/Analysis/AnalysisStruct.cpp
@@ -1,5 +1,22 @@
 #include "AnalysisStruct.h"
 
+#include "Beatmap\Velocity.h"
+#include "Beatmap\NoteRate.h"
+#include "Beatmap\NoteRatePerKey.h"
+#include "Beatmap\NoteRatePerKeyPrecise.h"
+#include "Beatmap\Overlaps.h"
+#include "Beatmap\Distance-size.h"
+#include "Beatmap\TapEventIntervals.h"
+
+#include "Beatmap\Difficulty\Reading.h"
+
+#include "Replay\PressReleaseIntervals.h"
+#include "Replay\aim-deviation.h"
+#include "Replay\tap-deviation.h"
+
+#include "Score\AimScore.h"
+#include "Score\TapScore.h"
+
 Analysis AnalysisStruct::beatmapAnalysis;
 Analysis AnalysisStruct::replayAnalysis;
 Analysis AnalysisStruct::scorepAnalysis;
--- osu-Replay-Analyzer/osu!/Analysis/AnalysisStruct.h
+++ osu-Replay-Analyzer/osu!/Analysis/AnalysisStruct.h
@@ -3,22 +3,6 @@
 
 #include "Analysis.h"
 
-#include "Beatmap\Velocity.h"
-#include "Beatmap\NoteRate.h"
-#include "Beatmap\NoteRatePerKey.h"
-#include "Beatmap\NoteRatePerKeyPrecise.h"
-#include "Beatmap\Overlaps.h"
-#include "Beatmap\Distance-size.h"
-#include "Beatmap\TapEventIntervals.h"
-
-#include "Beatmap\Difficulty\Reading.h"
-
-#include "Replay\aim-deviation.h"
-#include "Replay\tap-deviation.h"
-
-#include "Score\AimScore.h"
-#include "Score\TapScore.h"
-
 struct AnalysisStruct
 {
 	static Analysis beatmapAnalysis;
