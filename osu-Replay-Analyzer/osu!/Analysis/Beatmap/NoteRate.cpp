88221919d8a9f0313c25bf7f474fc4973d0d77c9
abraker95 <abraker95@gmail.com>
2016-08-31 01:32:50 -0400

    [FIX] Parse note rate by note instead of by time intervals

--- osu-Replay-Analyzer/osu!/Analysis/Beatmap/NoteRate.cpp
+++ osu-Replay-Analyzer/osu!/Analysis/Beatmap/NoteRate.cpp
@@ -54,38 +54,56 @@ void Analyzer_NoteRate::AnalyzeTaiko(Play* _play)
 
 void Analyzer_NoteRate::AnalyzeMania(Play* _play)
 {
-	int KEYS = _play->beatmap->getMods().getCS();
-
 	std::vector<Hitobject*>& hitobjects = _play->beatmap->getHitobjects();
+	int KEYS = _play->beatmap->getMods().getCS();
+	
 	osu::TIMING timing;
-	timing.data = 0;
+		timing.data = 0;
 
-	int i = 0, previ = 0;
-	double delay = 10.0;
+	long currTime = hitobjects[0]->getTime();
+	std::vector<long> holdnoteEnds;
+		holdnoteEnds.resize(KEYS);
 
-	for (int ms = 0; ms < hitobjects[hitobjects.size() - 1]->getTime(); ms += delay)
+	data.push_back({ currTime - 1, 0 }); // make the min be 0
+
+	int noteCount = 0;
+
+	for (int i = 0; i < hitobjects.size(); i++)
 	{
-		// Let index catch up to the time. Bail if out of bounds
-		while (hitobjects[i]->getEndTime() < ms) i++;
-		if (i >= hitobjects.size()) break;
+		if (hitobjects[i]->isHitobjectLong())
+			holdnoteEnds[OSUMANIA::getKey(hitobjects[i]->getPos().X, KEYS)] = hitobjects[i]->getEndTime();
 
-		// Out of bound check
-		if (!BTWN(1, i, hitobjects.size() - 1))
-			continue;
-
-		// previ - i needs to be at least 1 second apart
-		while (hitobjects[i]->getTime() - hitobjects[previ]->getTime() > 1000.0)
+		long hitobjectTime = hitobjects[i]->getTime();
+		if (hitobjectTime > currTime)
 		{
-			if (previ < hitobjects.size() - 1) previ++;
-			else break;
+			noteCount++;
+			if (i <= 1)
+				noteCount--;
+
+			// See if there are any hold notes at this time as well
+			for (long holdnoteEnd : holdnoteEnds)
+			{
+				if (BTWN(currTime, hitobjectTime, holdnoteEnd))
+					noteCount++;
+			}
+
+			timing.data = (double)noteCount / ((double)hitobjectTime - (double)currTime) * 1000.0;
+			timing.time = hitobjectTime;
+
+			// True = just presses, False = Presses and Releases
+			timing.press = true;
+			for (long holdnoteEnd : holdnoteEnds)
+				timing.press &= (holdnoteEnd == hitobjectTime);
+
+			data.push_back(timing);
+
+			currTime = hitobjects[i]->getTime();
+			noteCount = 0;
 		}
-
-		timing.data = i - previ;
-		timing.pos = vector2df(OSUMANIA::getKey(hitobjects[i]->getPos().X, KEYS), 0);
-		timing.press = false;
-		timing.time = ms;
-
-		data.push_back(timing);
+		else
+		{
+			noteCount++;
+		}
 	}
 }
 
