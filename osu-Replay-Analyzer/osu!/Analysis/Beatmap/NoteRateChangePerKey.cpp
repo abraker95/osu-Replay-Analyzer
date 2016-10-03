ab00672993bba9c59394eba31a1603c79eb8e265
abraker95 <abraker95@gmail.com>
2016-10-03 15:45:07 -0400

    [ADD] Note rate change analysis for std

--- osu-Replay-Analyzer/osu!/Analysis/Beatmap/NoteRateChangePerKey.cpp
+++ osu-Replay-Analyzer/osu!/Analysis/Beatmap/NoteRateChangePerKey.cpp
@@ -7,7 +7,26 @@
 Analyzer_NoteRateChangePerKey::Analyzer_NoteRateChangePerKey() : Analyzer("notes/s^2 per key") {}
 Analyzer_NoteRateChangePerKey::~Analyzer_NoteRateChangePerKey() {}
 
-void Analyzer_NoteRateChangePerKey::AnalyzeStd(Play* _play){}
+void Analyzer_NoteRateChangePerKey::AnalyzeStd(Play* _play)
+{
+	Analyzer* analyzer = AnalysisStruct::beatmapAnalysis.getAnalyzer("note rate (notes/s)");
+	std::vector<osu::TIMING>& noteRates = *(analyzer->getData());
+	if (noteRates.size() == 0) return;
+
+	int KEYS = _play->beatmap->getMods().getCS();
+
+	osu::TIMING timing;
+	timing.data = 0;
+
+	for (int i = 1; i < noteRates.size(); i++)
+	{
+		timing.time = noteRates[i].time;
+		timing.data = noteRates[i].data - noteRates[i - 1].data;
+		timing.key = noteRates[i].key;
+
+		data.push_back(timing);
+	}
+}
 
 void Analyzer_NoteRateChangePerKey::AnalyzeCatch(Play* _play){}
 
