d304973a1b19a911d9d20c777e7ef163f430467a
abraker95 <abraker95@gmail.com>
2016-10-25 12:40:34 -0400

    [FIX] TappingControl v2

--- osu-Replay-Analyzer/osu!/Analysis/Beatmap/Difficulty/TappingControl.cpp
+++ osu-Replay-Analyzer/osu!/Analysis/Beatmap/Difficulty/TappingControl.cpp
@@ -20,23 +20,28 @@ void Analyzer_TappingControl::AnalyzeStd(Play* _play)
 
 	osu::TIMING timing;
 		timing.data = 0;
+		timing.key = 0;
 
 	double rawControl = 0.0;
-	for (osu::TIMING noteRateChange : noteRateChanges)
+	for (int i = 1; i < noteRateChanges.size(); i++)
 	{
-		if (noteRateChange.data == INFINITY) continue;
-		int key = noteRateChange.key;
+		if (noteRateChanges[i].data == INFINITY) continue;
+		int key = noteRateChanges[i].key;
 
-		osu::TIMING noteRate = noteRates[osu::FindTimingAt(noteRates, noteRateChange.time)];
+		osu::TIMING noteRate = noteRates[osu::FindTimingAt(noteRates, noteRateChanges[i].time)];
 
-		if (noteRateChange.data > 0)	  rawControl += 0.90*noteRateChange.data*noteRate.data;
-		else if (noteRateChange.data < 0) rawControl -= 0.70*rawControl * (1.0 / noteRate.data);
-		else							  rawControl -= 0.50*rawControl * (1.0 / noteRate.data);
+		const double MAX_INFLUENCE = 0.9;
+		double influencers = noteRate.data /* pow(noteRateChanges[i - 1].data / 10.0, 10)*/;
+		double influence = MAX_INFLUENCE * (-exp(-influencers / 20.0) + 1);
+		
+		if (noteRateChanges[i].data > 0) rawControl += 0.9 * noteRateChanges[i].data;
+		if (noteRateChanges[i].data < 0) rawControl -= 0.10*rawControl;
+		else							 rawControl -= 0.25*rawControl;
 
 		timing.data = rawControl;
-		timing.key = noteRateChange.key;
-		timing.pos = noteRateChange.pos;
-		timing.time = noteRateChange.time;
+		timing.key = MAX(noteRateChanges[i].key, rawControl);
+		timing.pos = noteRateChanges[i].pos;
+		timing.time = noteRateChanges[i].time;
 
 		data.push_back(timing);
 	}
@@ -72,7 +77,7 @@ void Analyzer_TappingControl::AnalyzeMania(Play* _play)
 
 		osu::TIMING noteRate = noteRates[osu::FindTimingAt(noteRates, noteRateChange.time)];
 
-		if (noteRateChange.data > 0)	 rawControls[key] += 0.90*noteRateChange.data*noteRate.data;
+		if (noteRateChange.data > 0)	  rawControls[key] += 0.90*noteRateChange.data*noteRate.data;
 		else if(noteRateChange.data < 0) rawControls[key] -= 0.70*rawControls[key] * (1.0/noteRate.data);
 		else							 rawControls[key] -= 0.50*rawControls[key] * (1.0/noteRate.data);
 
